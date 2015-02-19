af13d87c58005265c1c6a3e1f74238fe03421422
kyasuda2003 <kyasuda2003@gmail.com>
2015-02-18 23:36:28 -0800

    modify app.config.js

--- templates/psi/js/app.conf.js
+++ templates/psi/js/app.conf.js
@@ -2,10 +2,10 @@
  var app={};
   app.settings = {
     dev: true,
-    apipath: ["/obj",""][1],
-    apihost: ["http://localhost:9000", ""][1],
+    apipath: ["/obj",""][0],
+    apihost: ["http://localhost:9000", ""][0],
     mediapath: ["/media"][0],
-    apppath: ["/psi/", "/assets/"][1]
+    apppath: ["/psi/", "/assets/"][0]
   };
   window.app=app;
 
